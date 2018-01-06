574e94eb86db05c631cc23e8dbcb5f8018c94e6f
Shuto Shikama <shuto@ShutonoMac-mini.local>
2018-01-06 19:48:22 +0900

    ハーフエッジ用の構造体を追加

--- include/gal/surface.hpp
+++ include/gal/surface.hpp
@@ -13,10 +13,29 @@
 
 namespace gal
 {
+    struct Face;
+    struct Halfedge;
+    struct Vertex;
+    
+    struct Face
+    {
+        Halfedge* pHalfedge{nullptr};
+    };
+    
+    struct Halfedge
+    {
+        Vertex* pBegin{nullptr};
+        Face* pFace{nullptr};
+        Halfedge* pPair{nullptr};
+        Halfedge* pNext{nullptr};
+        Halfedge* pPrevious{nullptr};
+    };
+    
     struct Vertex
     {
         gal::Vector Position;
-        gal::Vector Normal;
+        float* pLocation{nullptr};
+        Halfedge* pHalfedge{nullptr};
     };
     
     class Surface
@@ -31,6 +50,10 @@ namespace gal
         std::vector<std::uint32_t>& getIndices(){ return m_indices; }
         
     protected:
+        std::vector<Halfedge> m_halfedges;
+        std::vector<Face> m_faces;
+        std::vector<Vertex> m_vertice;
+        
         std::vector<float> m_vertices;
         std::vector<std::uint32_t> m_indices;
     };
