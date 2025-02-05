ba9ac7042ee1c7a742b3a9badc91d53e2427f1d1
Shuto Shikama <@gmail.com>
2025-02-05 15:46:11 +0900

    Material UI

--- girly/frontend/src/main.tsx
+++ girly/frontend/src/main.tsx
@@ -1,12 +1,10 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-const App = () => {
-    return (<p>My App</p>);
-}
-
-createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+createRoot(document.getElementById("root")! as HTMLElement).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
